757fb33ab4a8268e74a5e4a690078bb8aae7f409
Marcelo Escobar <escobar@br.systems>
2019-07-23 12:30:53 -0300

    changed

--- app/controllers/gmail_controller.rb
+++ app/controllers/gmail_controller.rb
@@ -4,7 +4,41 @@ require "googleauth/stores/file_token_store"
 require "fileutils"
 
 class GmailController < ApplicationController
+    before_action :set_user, only: [:index, :show]
+
     def index
+    end
+    ##
+    # Ensure valid credentials, either by restoring from the saved credentials
+    # files or intitiating an OAuth2 authorization. If authorization is required,
+    # the user's default browser will be launched to approve the request.
+    #
+    # @return [Google::Auth::UserRefreshCredentials] OAuth2 credentials
+
+    def show
+        @msg_id = params[:id]
+    end
+
+    def authorize
+        client_id = Google::Auth::ClientId.from_file @credentials_path
+        token_store = Google::Auth::Stores::FileTokenStore.new file: @token_path
+        authorizer = Google::Auth::UserAuthorizer.new client_id, @scope, token_store
+        user_id = "default"
+        credentials = authorizer.get_credentials user_id
+        if credentials.nil?
+        @url = authorizer.get_authorization_url base_url: @oob_uri
+        puts "Open the following URL in the browser and enter the " \
+                "resulting code after authorization:\n" + @url
+        code = gets
+        credentials = authorizer.get_and_store_credentials_from_code(
+            user_id: user_id, code: code, base_url: @oob_uri
+        )
+        end
+        credentials
+    end
+
+private
+    def set_user
         @oob_uri = "urn:ietf:wg:oauth:2.0:oob".freeze
         @application_name = "Gmail Viewer".freeze
         @credentials_path = "credentials.json".freeze
@@ -20,38 +54,5 @@ class GmailController < ApplicationController
         @service.authorization = authorize
 
         @user_id = "me"
-    end
-    ##
-    # Ensure valid credentials, either by restoring from the saved credentials
-    # files or intitiating an OAuth2 authorization. If authorization is required,
-    # the user's default browser will be launched to approve the request.
-    #
-    # @return [Google::Auth::UserRefreshCredentials] OAuth2 credentials
-
-    def show
-        raise
-        id = params[:id]
-    end
-
-    def authorize
-        client_id = Google::Auth::ClientId.from_file @credentials_path
-        token_store = Google::Auth::Stores::FileTokenStore.new file: @token_path
-        authorizer = Google::Auth::UserAuthorizer.new client_id, @scope, token_store
-        user_id = "default"
-        credentials = authorizer.get_credentials user_id
-        if credentials.nil?
-        @url = authorizer.get_authorization_url base_url: @oob_uri
-        # puts "Open the following URL in the browser and enter the " \
-        #         "resulting code after authorization:\n" + url
-        # code = gets
-        code = new_code
-        credentials = authorizer.get_and_store_credentials_from_code(
-            user_id: user_id, code: code, base_url: @oob_uri
-        )
-        end
-        credentials
-    end
-
-    def new_code
     end
 end
